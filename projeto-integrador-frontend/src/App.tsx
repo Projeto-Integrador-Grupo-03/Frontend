b0247fe6564b882b85d30b247a993b2de17711b3
Hiago Oliveira <hiagosantos223@gmail.com>
2024-05-02 12:44:27 -0300

    Projeto Vite criado

---
+++ projeto-integrador-frontend/src/App.tsx
@@ -0,0 +1,16 @@
+// import Footer from './components/Footer/Footer'
+// import Navbar from './components/NavBar/NavBar'
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Navbar from './components/NavBar/NavBar'
+
+function App() {
+  return (
+    <>
+   <Navbar/>
+   <Footer/>
+    </>
+  )
+}
+
+export default App
