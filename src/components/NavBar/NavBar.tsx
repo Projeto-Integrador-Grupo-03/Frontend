ee6637c4d6cf554be1b996a7ec61f30fe3b99185
Henrique_zz3 <henriquetoto3001@gmail.com>
2024-05-07 11:39:30 -0300

    Atualizações

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthContext';
 
 
 function Navbar() {
- 
+  
+  let navigate = useNavigate()
+
+  const { usuario, handleLogout } = useContext(AuthContext)
+
+  function logout() {
+    handleLogout()
+    alert('Usuário deslogado com sucesso')
+    navigate('/login')
+}
+
   return (
     <>
      <div className='w-full bg-black text-white flex justify-center py-4'>
@@ -15,7 +26,7 @@ function Navbar() {
               <Link to='/home' className='hover:underline'>Home</Link>
               <Link to='/sobre' className='hover:underline'>Sobre nós</Link>
               <div className='hover:underline'>Perfil</div>
-              <div className='hover:underline'>Sair</div>
+              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
             </div>
           </div>
         </div>
