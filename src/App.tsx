827efca40e182fc9db2e0f05447d1a79215e6c28
Henrique_zz3 <henriquetoto3001@gmail.com>
2024-05-07 11:23:10 -0300

    Ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Login from './paginas/login/Login';
 import Cadastro from './paginas/cadastro/Cadastro';
 import Home from './paginas/home/Home';
 import { AuthProvider } from './contexts/AuthContext';
+import Sobre from './paginas/sobre/Sobre';
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/sobre" element={<Sobre />} />
             </Routes>
           </div>
           <Footer />
