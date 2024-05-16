14436bd8a64209256807a954de37e8083ac84215
Luiz Pascucho <luizpascucho@bol.com.br>
2024-05-16 09:38:35 -0300

    Deploy do Projeto Integrador

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://backend-4066.onrender.com/'
+  baseURL: import.meta.env.VITE_API_URL
 })
 
 export const cadastrarUsuario = async(url: string, dados: Object, setDados: Function) => {
