7b4f89f5560892b4cdc38bcf173ad11ae14a5f2d
Hiago Oliveira <hiagosantos223@gmail.com>
2024-05-17 12:27:12 -0300

    up sobre nos 02

--- src/paginas/sobre/Sobre.tsx
+++ src/paginas/sobre/Sobre.tsx
@@ -5,19 +5,24 @@ import Card from './CardSobre';
 function Sobre() {
     return (
         <>
-        <div className="text-white flex justify-center font-bold">
-            <div>Nossos Valores</div>
-            <div>
-                <h3>Na SustentaDrive, estamos comprometidos com um futuro mais limpo e sustentável para todos. Desde o nosso início, nos dedicamos a oferecer uma alternativa revolucionária na indústria automobilística: veículos que não apenas entregam desempenho excepcional, mas também protegem o planeta que todos chamamos de lar.
-               Na SustentaDrive, acreditamos que o progresso não precisa vir às custas do meio ambiente. É por isso que nos especializamos em veículos elétricos e outras tecnologias de mobilidade sustentável. Nossos carros são alimentados por energia limpa e renovável, reduzindo drasticamente as emissões de carbono e promovendo a saúde do nosso ecossistema.
-                Além disso, estamos comprometidos em fornecer uma experiência excepcional aos nossos clientes. Nossa equipe dedicada está aqui para ajudá-lo a encontrar o veículo perfeito para atender às suas necessidades, ao mesmo tempo em que orienta você através das opções de financiamento e assistência técnica.
-                Na SustentaDrive, acreditamos que cada escolha conta. Ao escolher um de nossos veículos, você não apenas desfruta de uma condução suave e eficiente, mas também contribui para um futuro mais limpo e saudável para as gerações futuras. Junte-se a nós enquanto avançamos em direção a um mundo onde a mobilidade ecológica é a norma.
-                Obrigado por escolher SustentaDrive. Juntos, estamos dirigindo para um amanhã melhor.</h3>
+        <div className="text-white font-bold flex flex-col items-center">
+            <div className="mb-4 text-center">Nossos Valores</div>
+            <div className="text-center">
+                <h3 className="text-center">
+                    Na SustentaDrive, estamos comprometidos com um futuro mais limpo e sustentável para todos. Desde o nosso início, nos dedicamos a oferecer uma alternativa revolucionária na indústria automobilística: veículos que não apenas entregam desempenho excepcional, mas também protegem o planeta que todos chamamos de lar.
+                    Na SustentaDrive, acreditamos que o progresso não precisa vir às custas do meio ambiente. É por isso que nos especializamos em veículos elétricos e outras tecnologias de mobilidade sustentável. Nossos carros são alimentados por energia limpa e renovável, reduzindo drasticamente as emissões de carbono e promovendo a saúde do nosso ecossistema.
+                    Além disso, estamos comprometidos em fornecer uma experiência excepcional aos nossos clientes. Nossa equipe dedicada está aqui para ajudá-lo a encontrar o veículo perfeito para atender às suas necessidades, ao mesmo tempo em que orienta você através das opções de financiamento e assistência técnica.
+                    Na SustentaDrive, acreditamos que cada escolha conta. Ao escolher um de nossos veículos, você não apenas desfruta de uma condução suave e eficiente, mas também contribui para um futuro mais limpo e saudável para as gerações futuras. Junte-se a nós enquanto avançamos em direção a um mundo onde a mobilidade ecológica é a norma.
+                    Obrigado por escolher SustentaDrive. Juntos, estamos dirigindo para um amanhã melhor.
+                </h3>
             </div>
         </div>
 
+        <hr/>
+        <hr/>
+
         <div className="text-white flex justify-center font-bold">
-        Integrantes do Projeto:
+            <h1>Integrantes do Projeto:</h1>
         </div>
         
         <div className=" flex justify-center">   
